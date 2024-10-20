7c23e5a6b2e8048ffbf08ec45337f7818a05779c
Mushabberul Islam <dev.sabbirmia@gmail.com>
2024-10-20 08:19:12 +0600

    variable

---
+++ variable.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+    // Calculation of memory size 
+        // 1bit=0/1
+        // 1byte = 4bit
+        // 1kb = 1024byte
+        // 1mb = 1024kb
+        // 1gb = 1024mb
+
+    // 4 kind of data type in C 
+        // 1. integer = int 
+        // 2. float = float
+        //3. charector = char
+        //4. boolean = bool
+
+    // integer & float grab 4bype in RAM
+    // Declear 
+    //     int name
+    //Assign
+        // int name = 10;
+
+    // int number = 10;
+    // printf("%d",number);
+
+    float TestFloat = 3.40;
+    printf("%0.2f",TestFloat);
+
+    return 0;
+}
