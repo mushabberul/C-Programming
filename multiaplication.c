47cc068dbf218acd948d123737ed9ea487d52afe
Mushabberul Islam <dev.sabbirmia@gmail.com>
2024-10-23 20:14:28 +0600

    divisation and multiapplication

---
+++ multiaplication.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    long long int a,b,multiaplication;
+    scanf("%lld %lld",&a,&b);
+    multiaplication = a * b;
+    printf("%lld",multiaplication);
+    return 0;
+}
