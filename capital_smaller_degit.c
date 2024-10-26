fe2db13cea0002f19bf0e887b5fd00fe046d8871
Mushabberul Islam <dev.sabbirmia@gmail.com>
2024-10-26 22:38:26 +0600

    solve some problem

---
+++ capital_smaller_degit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    char a;
+    scanf("%c",&a);
+    
+    if(a>='0' && a<='9'){
+        printf("IS DIGIT");
+    }else if(a>='A' && a<='Z'){
+        printf("ALPHA \nIS CAPITAL");
+    }else if(a>='a' && a<='z'){
+        printf("ALPHA \nIS SMALL");
+    }else{
+        printf("Special Charactor");
+    }
+    return 0;
+}
